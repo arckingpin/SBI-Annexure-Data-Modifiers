b04b67e3c0d316bc4bd63349dcb0071f650918e9
arckingpin <42000966+arckingpin@users.noreply.github.com>
2025-03-11 14:13:30 +0530

    Add files via upload

---
+++ 1_Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Welcome to My SBI Annexure Data Processing and modification App!")
+st.write("This is the homepage. Navigate to other pages using the sidebar.")
+
+
